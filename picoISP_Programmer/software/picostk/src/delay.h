12524175d7547f8eff3c04bc3ae5dfee4ed9f37f
wagiminator <wagiminator@web.de>
2023-11-19 09:07:53 +0100

    More precise delays (issue #4)

--- picoISP_Programmer/software/picostk/src/delay.h
+++ picoISP_Programmer/software/picostk/src/delay.h
@@ -1,8 +1,9 @@
 // ===================================================================================
-// Delay Functions for CH551, CH552 and CH554
+// Delay Functions for CH551, CH552 and CH554                                 * v1.1 *
 // ===================================================================================
 
 #pragma once
 #include <stdint.h>
+#include "ch554.h"
 
 void DLY_us(uint16_t n);   // delay in units of us
